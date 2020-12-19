724e295b04b2203b134ca6688566f876c35393b6
sakkamade <sakkamade@users.noreply.github.com>
2020-12-19 15:17:29 +0300

    0.5.2 (source-han-serif update,scroll-btn update )

--- src/js/scroll-button.js
+++ src/js/scroll-button.js
@@ -1,3 +1,64 @@
+  // Code goes here
+// https://github.com/krvikash35/js-auto-hide-header-onscroll
+var lastKnownScrollY = 0;
+var currentScrollY = 0;
+var ticking = false;
+var idOfHeader = 'scroll-btn';
+var eleHeader = null;
+
+
+const classes = {
+  pinned: 'btn-pin',
+  unpinned: 'btn-unpin',
+};
+
+
+function onScroll() {
+  currentScrollY = window.pageYOffset;
+  requestTick();
+}
+
+
+function requestTick() {
+  if (!ticking) {
+    requestAnimationFrame(update);
+  }
+  ticking = true;
+}
+
+function update() {
+  if (currentScrollY < lastKnownScrollY) {
+    pin();
+  } else if (currentScrollY > lastKnownScrollY) {
+    unpin();
+  }
+  lastKnownScrollY = currentScrollY;
+  ticking = false;
+}
+
+
+function pin() {
+  if (eleHeader.classList.contains(classes.unpinned)) {
+    eleHeader.classList.remove(classes.unpinned);
+    eleHeader.classList.add(classes.pinned);
+  }
+}
+
+
+function unpin() {
+  if (eleHeader.classList.contains(classes.pinned) || !eleHeader.classList.contains(classes.unpinned)) {
+    eleHeader.classList.remove(classes.pinned);
+    eleHeader.classList.add(classes.unpinned);
+  }
+}
+
+window.onload = function(){
+  eleHeader = document.getElementById(idOfHeader);
+  console.log(eleHeader);
+  document.addEventListener('scroll', onScroll, false);
+}
+
+// w3school
 //Get the button
 var mybutton = document.getElementById("scroll-btn");
 
@@ -5,7 +66,7 @@ var mybutton = document.getElementById("scroll-btn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
